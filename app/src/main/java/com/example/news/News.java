65ec93820ab3e4a768135338f96fb61bc9cac99f
github <pragya.sharma2_cs19@gla.ac.in>
2021-06-22 19:20:29 +0530

    create constructor, getter, setter.

--- app/src/main/java/com/example/news/News.java
+++ app/src/main/java/com/example/news/News.java
@@ -1,5 +1,30 @@
 package com.example.news;
 
-public class
-News {
+public class News {
+    String title, desc;
+
+    public News(String title, String desc) {
+        this.title = title;
+        this.desc = desc;
+    }
+
+    public News() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
 }
