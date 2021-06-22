de5c43244db3362eb78dbf12383d3e42fe4765e1
github <pragya.sharma2_cs19@gla.ac.in>
2021-06-22 19:28:07 +0530

    create newsitem.xml

--- app/src/main/java/com/example/news/MainActivity.java
+++ app/src/main/java/com/example/news/MainActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        News news1 = new News("covid-19", "Warning for the third wave");
+        News news2 = new News("ANdroid", "New Feature");
+        List<News> newsList = new ArrayList<>();
+        newsList.add(news1);
+        newsList.add(news2);
     }
 }
