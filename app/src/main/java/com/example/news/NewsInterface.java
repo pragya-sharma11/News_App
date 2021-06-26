56736ba36cd4a9a5d472300f8c7de4c2715d5f28
github <pragya.sharma2_cs19@gla.ac.in>
2021-06-26 23:58:17 +0530

    add category

--- app/src/main/java/com/example/news/NewsInterface.java
+++ app/src/main/java/com/example/news/NewsInterface.java
@@ -7,9 +7,7 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface NewsInterface {
-    String s1="v2/top-headlines?country=in&category=", s2="technology";
     //everything after ? is query so retrofit has an annotation called query
-    String s3="&apiKey=3ecb725ad3614d879cb6b89c9974a733";
     @GET("v2/top-headlines?country=in&apiKey=3ecb725ad3614d879cb6b89c9974a733") //given end point.
     Call<NewsModel> getNewsData(@Query("category") String category); //here parameter category will be set in category query .
 
